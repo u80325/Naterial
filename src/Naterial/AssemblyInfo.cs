f9ddeeb90fe833ab83d4ad725689764fb4ae851b
u80325 <162207312+u80325@users.noreply.github.com>
2024-03-06 22:29:48 +0300

    feat: add solution with library & demo project

---
+++ src/Naterial/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: XmlnsPrefix("http://schemas.naterial.com/winfx/xaml", "nd")]
+
+[assembly: ComVisible(false)]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
+)]
